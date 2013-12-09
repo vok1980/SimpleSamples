74a19df6a161dd5d12e3323574d7e6fa7fe83a22
Vyacheslav O. Koshcheyev <v.koscheev@socialquantum.ru>
2013-12-09 11:48:09 +0700

    implement bit changer caller

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
 
+#include <time.h>
+#include <stdlib.h>
+#include "changebit.h"
+
+
 
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	char c = 0xff;
+    
+    srand (time(NULL));
+    
+    for (int i = 0; i < 20; ++i)
+    {
+        int iBitNum = rand() % 10 - 1;
+        ChangeBit(&c, iBitNum);
+    }
+
 	return 0;
 }
